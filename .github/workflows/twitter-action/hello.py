2bc14865926dfd0e59e1b6ce28ae0419ab200318
Kexin Gu <kexin.gu17@imperial.ac.uk>
2022-11-10 14:15:06 +0000

    Test if condition in action

--- .github/workflows/twitter-action/hello.py
+++ .github/workflows/twitter-action/hello.py
@@ -1,3 +1,15 @@
-import tweepy
+import os
 
-print('Hello, import tweepy worked somehow :P')
+TWITTER_APP_KEY = os.environ["TWITTER_APP_KEY"]
+TWITTER_APP_SECRET = os.environ["TWITTER_APP_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+
+if TWITTER_APP_KEY:
+    print("Got app key")
+if TWITTER_APP_SECRET:
+    print("Got app secret")
+if ACCESS_TOKEN:
+    print("Got access token")
+if ACCESS_TOKEN_SECRET:
+    print("Got access token secret")
